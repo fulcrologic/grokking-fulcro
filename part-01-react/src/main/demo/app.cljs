e94a35bb04ce7dfc77f1e9f0935035e1dd30be2a
Tony Kay <tony.kay@gmail.com>
2021-02-06 17:51:10 -0800

    setup

--- part-01-react/src/main/demo/app.cljs
+++ part-01-react/src/main/demo/app.cljs
@@ -1,6 +1,54 @@
 (ns demo.app
   (:require
+    [com.fulcrologic.fulcro.dom :refer [div li ol h1]]
     [cljsjs.react]
     [cljsjs.react.dom]))
 
-(defn f [x] x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(def root (.getElementById js/document "app"))
+
+(defn render! [element]
+  (js/ReactDOM.render element root))
+
+
+(defn items [items]
+  (ol
+    (map-indexed (fn [idx txt] (li {:key idx} txt)) items)))
+
+
+
+
+
+
+(comment
+
+
+  (def a "Item 1")
+  (macroexpand '(div "hello"))
+  (render! (.createElement js/React "div" #js {} #js ["Hello "]))
+  (render! (.createElement js/React "div" #js {} #js ["Hello There"]))
+  (render! (.createElement js/React "div" #js {"className" "red"} #js ["Hello There Bob"]))
+
+  (render! (div
+             (h1 "Items")
+             (items ["a" "OTHER" "c"])))
+
+  (time
+    (dotimes [n 100000]
+      (render! (div nil a))))
+
+  )
+
